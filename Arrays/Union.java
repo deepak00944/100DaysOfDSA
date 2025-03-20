0d62ecdb8f1f7cbef22b4eb42570c59872a71cc6
deepak00944 <deepak.s.ashta@gmail.com>
2025-03-20 23:39:12 +0530

    added

--- Arrays/Union.java
+++ Arrays/Union.java
@@ -1,5 +1,10 @@
 class Union {
-    psvm
+    public static void main(String[] args){
+        int[] a = {1,2,3};
+        int[] b = {1,2,3,4,5};
+        int res = findUnion(a, b);
+        System.out.println(res);
+    }
     public static int findUnion(int a[], int b[]) {
         int[] cnt = new int[(int)(1e5+5)];
         int res=0;
