ac4fe9163acea9a08b71c74dc07aa3582c7e79a2
deepak00944 <deepak.s.ashta@gmail.com>
2025-03-25 13:26:18 +0530

    kaddens

--- Algorithms/Kadanes.java
+++ Algorithms/Kadanes.java
@@ -9,15 +9,17 @@
 In this problem, we have to find the maximum subarray sum. We can solve this problem using Kadane’s algorithm. Here, we will initialize two variables max and res with 0. Then we will iterate over the array
 and for each element, we will update max as the maximum of (max+arr[i], arr[i]) and res as the maximum of (max, res). At the end of the iteration, res will have the maximum subarray sum.  */
 
-public class kadanes{
+public class Kadanes{
     public static void main(String[] args){
-        int[] arr = {5, 4, 1, 7, 8};
-        int max=0,res=0;
+        int[] arr = {-2,-4};
+        
+        int max =arr[0], res =arr[0];
 
-        for (int i = 0; i < arr.length; i++) {
-            max = Math.max(max+arr[i], arr[i]);
-            res = Math.max(max,res);
-        }
+            for(int i=1; i<arr.length; i++){
+                max = Math.max(max+arr[i], arr[i]);
+                res = Math.max(max, res);
+            }
+        
         System.out.println(res);
     }
 }
