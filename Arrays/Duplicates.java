eff52a3d45a8846f55e12e5b32e565d51c606fdd
deepak00944 <deepak.s.ashta@gmail.com>
2025-04-01 12:04:59 +0530

    Printing duplicates in asrray using brute force and optimized approach

---
+++ Arrays/Duplicates.java
@@ -0,0 +1,40 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Duplicates {
+    public static void main(String[] args) {
+        int arr[] = {2, 3, 1, 2, 3,2};
+        System.out.println(duplicates(arr));
+    }
+
+    static List<Integer> duplicates(int[]arr){
+        List<Integer> list = new ArrayList<>(10);
+        int n = arr.length;
+        int[] freqArr = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            freqArr[arr[i]]++;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if(freqArr[arr[i]]>1){
+                list.add(arr[i]);
+                freqArr[arr[i]] =0;
+            }
+        }
+
+        // Brute Force
+        // for (int i = 0; i < arr.length-1; i++) {
+        //     if(arr[i]==arr[i+1]){
+        //         if(!list.contains(arr[i])){
+        //             list.add(arr[i]);
+        //         }
+        //     }
+        // }
+        
+
+        
+        return list;
+    }
+}
