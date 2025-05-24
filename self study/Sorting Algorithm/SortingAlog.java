2aa7a342ae7e43aac9db0143b64877e2922842be
deepak00944 <deepak.s.ashta@gmail.com>
2025-05-24 11:14:17 +0530

    Bubble sort

--- self study/Sorting Algorithm/SortingAlog.java
+++ self study/Sorting Algorithm/SortingAlog.java
@@ -3,11 +3,12 @@ import java.util.Arrays;
 public class SortingAlog {   
     public static void main(String[] args) {
         int [] arr = {5,3,9,0,2};
-        sort1(arr);
+        selection_sort(arr);
+        bubble_sort(arr);
     }
 
-    // Selection sort
-    public static void sort1(int [] arr){
+    // Selection sort (O(n^2))
+    public static void selection_sort(int [] arr){
         for (int i = 0; i < arr.length-1; i++) {
             int min = i;
             for (int j = i+1; j < arr.length; j++) {
@@ -15,11 +16,28 @@ public class SortingAlog {
                     min = j;
                 }
             }
-
+            // swap
             int temp = arr[min];
             arr[min] = arr[i] ;
             arr[i] = temp;
         }
         System.out.println(Arrays.toString(arr));
     }
+
+    // Bubule sort
+    public static void bubble_sort(int[] arr){
+        int n = arr.length;
+        for (int i = n-1; i >0; i--) {
+            for (int j = 0; j <= i-1; j++) {
+                if(arr[j]> arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        System.out.println("Bubble sort: "+Arrays.toString(arr));
+    }
+
+
 }
