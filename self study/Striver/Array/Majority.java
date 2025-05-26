0740ca91e0c358d9e3d6fdfad4a1ad34ff8611b3
deepak00944 <deepak.s.ashta@gmail.com>
2025-05-26 10:32:40 +0530

    prob 3 (med)

---
+++ self study/Striver/Array/Majority.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Majority {
+    public static void main(String[] args) {
+        int [] nums = {2,2,1,1,1,2,2};
+        System.out.println(majorityElement(nums));
+    }
+
+    public static int majorityElement(int[] nums) {
+        // Arrays.sort(nums);
+        // int n = nums.length;
+        // return nums[n/2];
+
+        int n = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < n; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+        
+        n = n / 2;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > n) {
+                return entry.getKey();
+            }
+        }
+        
+        return 0;
+    }
+}
